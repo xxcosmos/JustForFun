5e823adc731c32685aecb7e9f7bff3f7eb1cac9e
xxcosmos <xandcosmos@gmail.com>
2019-04-23 13:50:09 +0800

    爬取大学生互联网+项目列表

---
+++ src/main/java/File/CreateFile.java
@@ -0,0 +1,31 @@
+package File;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CreateFile {
+    public static void createDir(String pathName, String studentId) {
+        File file = new File(pathName);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        File picture = new File(pathName + "/" + studentId + ".jpg");
+        if (picture.exists())
+            return;
+        else {
+            try {
+                picture.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        String s = "201713137042";
+        System.out.println(s.substring(4, 6));
+        //createDir("/Users/xiaoyuu/IdeaProjects/JustForFun/src/main/resources/2017/13/137","201713137042");
+    }
+}
---
+++ src/main/java/NCSS/GetAllProject.java
@@ -0,0 +1,39 @@
+package NCSS;
+
+import org.springframework.web.client.RestTemplate;
+import utils.GetARestTemplate;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class GetAllProject {
+    public static final String BASE_URL = "https://cy.ncss.org.cn/search/projectlist?name=&industryCode=&wasBindUniTechnology=-9&investStageCode=&provinceCode=";
+    public static final int PROJECT_NUM = 592896;
+    public static final String PATH_NAME = "/Users/xiaoyuu/IdeaProjects/JustForFun/src/main/resources/project_list.txt";
+
+    static void getProject() throws IOException {
+        int pageSize = 1000;
+        File file = new File(PATH_NAME);
+        FileWriter fileWriter = new FileWriter(file);
+        fileWriter.write("");
+        for (int i = 0; i < (PROJECT_NUM / pageSize) + 1; i++) {
+            String listUrl = BASE_URL + "&pageIndex=" + i + "&pageSize=" + pageSize;
+            RestTemplate restTemplate = GetARestTemplate.getInstance();
+            String response = restTemplate.getForObject(listUrl, String.class);
+            fileWriter.append(response);
+        }
+        fileWriter.close();
+    }
+
+    public static void main(String[] args) {
+        try {
+            getProject();
+//            File file = new File(PATH_NAME);
+//            FileWriter fileWriter = new FileWriter(file);
+//            fileWriter.write("");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
